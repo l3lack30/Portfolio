6fcedc40c442bb55dc1d3003a7647f9940ddcbe8
l3lack30 <l3lack30@gmail.com>
2023-12-10 20:44:24 +0700

    init project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="t px-4 py-5 my-5 text-center">
+  <div class="t px-4 py-5 my-5 text-center" id="Home">
     <img class="d-block mx-auto mb-0 py-5 my-5" src="/src/assets/img/S__4120578.png" alt="" width="350">
     <h1 class="display-3 fw-bold text-uppercase">Sarayut Senarsa</h1>
     <h2 class="text-uppercase">Computer Science</h2>
