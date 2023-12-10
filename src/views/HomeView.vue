1cd37115cdaa3226e8419d07ab43ec7a61e3da42
l3lack30 <l3lack30@gmail.com>
2023-12-10 20:42:47 +0700

    init project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,8 @@
 <template>
-  <div class="t px-4 py-3 my-3 text-center" id="Home">
-    <img class="d-block mx-auto mb-0 py-5 my-5" src="/src/assets/img/S__4120578.png" alt="" width="450">
+  <div class="t px-4 py-5 my-5 text-center">
+    <img class="d-block mx-auto mb-0 py-5 my-5" src="/src/assets/img/S__4120578.png" alt="" width="350">
     <h1 class="display-3 fw-bold text-uppercase">Sarayut Senarsa</h1>
-    <h1>Computer Science</h1>
-    <div class="col-lg-7 mx-auto">
-      <p class="lead mb-4">สวัสดีครับ ผม นายศรายุทธ เสนอาสา นักศึกษามหาวิทยาลัยแม่โจ้ คณะวิทยาศาสตร์ สาขาวิทยาการคอมพิวเตอร์
-มีความสนใจในเรื่องเกี่ยวกับการเขียนโปรแกรมและการพัฒนาซอฟต์แวร์ จุดมุ่งหมายที่สำคัญของ
-ผมคือต้องการที่จะได้รับความรู้ใหม่ๆและประสบการณ์จากผู้มีประสบการณ์ก่อนที่จะได้ออกไปทำงานจริงและสุดท้ายนี้ผมพร้อมที่จะเรียนรู้สิ่งใหม่ๆ เพื่อให้ตัวเองได้มีการพัฒนายิ่งขึ้นไปอีก</p>
-    </div>
+    <h2 class="text-uppercase">Computer Science</h2>
   </div>
 </template>
 
